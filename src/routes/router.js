77b5ea56e873958a145fd520a4c7e1b1cd2ca0d2
vickoCodeHub <vic.eff3@gmail.com>
2023-05-31 12:32:46 +0700

    Update router.js added dependency

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
 //user endpoints
-const {coba} = require("../controllers/userController");
+const { coba } = require("../controllers/userController");
+
+router.get("/coba", coba);
 
 module.exports = router;
