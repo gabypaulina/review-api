9c60dc3847333f565a155d7c0a4f9f35cedd1f76
vickoCodeHub <76735916+gabypaulina@users.noreply.github.com>
2023-06-20 08:53:53 +0700

    Update adminController.js

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -20,8 +20,32 @@ const JWT_KEY = "proyek_ws";
 const admins = require("../models/admin")(sequelize, DataTypes);
 
 const register = async (req, res) => {
-  let schema = Joi.object({});
+  let schema = Joi.object({
+    username: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(5).max(8),
+  });
 };
+
+try {
+  await schema.validateAsync(req.body);
+} catch (error) {
+  if (error.toString().includes("empty")) {
+    return res.status(400).send({ message: "Invalid data field" });
+  }
+  if (error.toString().includes("username")) {
+    return res
+      .status(400)
+      .send({ message: "Username has already been taken" });
+  }
+  if (error.toString().includes("email")) {
+    return res.status(400).send({
+      message: "Invalid email address",
+    });
+  }
+
+  return res.status(400).send({ message: error.message });
+}
 
 let admin_db = await sequelize.query("select * from admin", {
   type: Sequelize.SELECT,
