f128559153f6caeacbc24ba9d293afcc03b3dcf9
vickoCodeHub <76735916+gabypaulina@users.noreply.github.com>
2023-06-30 19:52:58 +0700

    downgrade dependecies package following node js 16.6.1 version

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-const router = require("./src/routes/router");
-
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-
-app.use("/api", router);
